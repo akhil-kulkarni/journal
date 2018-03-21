c1ebf7af72aabcdfcb8f0cfb7109d3bf3dfa7fa9
Akhil Kulkarni <akhil16.4.93@gmail.com>
2018-03-21 20:01:26 +0530

    header search bar css changes

--- src/app/app-common/jnl-header/jnl-header.component.ts
+++ src/app/app-common/jnl-header/jnl-header.component.ts
@@ -2,43 +2,45 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-jnl-header',
-	// inputs: ['showHdrLogo', 'showSearchBar', 'showRightIconGroup'],
 	templateUrl: './jnl-header.component.html',
 	styleUrls: ['./jnl-header.component.scss'],
 })
 export class JnlHeaderComponent implements OnInit {
 
-
 	showHdrLogo: boolean;
 	showSearchBar: boolean;
 	showRightIconGroup: boolean;
 	showNotif: boolean;
+
 	constructor() {
-
-
-		// this.showSearchBar = (this.showSearchBarIn === 'true');
-		// this.showRightIconGroup = (this.showRightIconGroupIn === 'true');
-	}
-
-	@Input()
-	set showHdrLogoIn(showHdrLogoIn: string) {
-		console.log('showHdrLogoIn: ' + showHdrLogoIn);
-		this.showHdrLogo = (showHdrLogoIn === 'true');
-	}
-
-	@Input()
-	set showSearchBarIn(showSearchBarIn: string) {
-		console.log('showSearchBarIn: ' + showSearchBarIn);
-		this.showSearchBar = (showSearchBarIn === 'true');
-	}
-
-	@Input()
-	set showRightIconGroupIn(showRightIconGroupIn: string) {
-		console.log('showRightIconGroupIn: ' + showRightIconGroupIn);
-		this.showRightIconGroup = (showRightIconGroupIn === 'true');
 	}
 
 	ngOnInit() {
+	}
+
+	@Input()
+	set showHdrLogoIn(showHdrLogoIn: boolean) {
+		console.log('showHdrLogoIn: ' + showHdrLogoIn);
+		this.showHdrLogo = showHdrLogoIn;
+	}
+
+	@Input()
+	set showSearchBarIn(showSearchBarIn: boolean) {
+		console.log('showSearchBarIn: ' + showSearchBarIn);
+		this.showSearchBar = showSearchBarIn;
+	}
+
+	@Input()
+	set showRightIconGroupIn(showRightIconGroupIn: boolean) {
+		console.log('showRightIconGroupIn: ' + showRightIconGroupIn);
+		this.showRightIconGroup = showRightIconGroupIn;
+	}
+	get showRightIconGroupIn(): boolean {
+		return this.showRightIconGroup;
+	}
+
+	changeIcnGroupView() {
+		this.showRightIconGroupIn = !this.showRightIconGroupIn;
 	}
 
 }
